a3b3f13675b64c38ecb965a1c7382f22d2ec674f
lomofu <943034679@qq.com>
2019-05-26 15:49:16 +0800

    清仓

--- src/main/java/com/dao/MovieDao.java
+++
@@ -1,9 +0,0 @@
-package com.dao;
-
-import com.entity.Movie;
-
-import java.util.List;
-
-public interface MovieDao {
-    List<Movie> queryAllMovie();
-}
--- src/main/java/com/service/MovieTypeService.java
+++
@@ -1,9 +0,0 @@
-package com.service;
-
-import com.entity.MovieType;
-
-import java.util.List;
-
-public interface MovieTypeService {
-    List<MovieType> getAllMovieType();
-}
